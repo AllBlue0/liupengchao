8819b5e2d06d4126b101c6ea85eebc50f93ed77d
AllBlue0 <91538775+AllBlue0@users.noreply.github.com>
2022-01-02 16:54:12 +0800

    Create main.c

---
+++ LeetCode/remove-element/main.c
@@ -0,0 +1,15 @@
+// 时间复杂度：O(n)
+// 空间复杂度：O(1)
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slowIndex = 0; 
+        for (int fastIndex = 0; fastIndex < nums.size(); fastIndex++) {  
+            if (val != nums[fastIndex]) { 
+                nums[slowIndex++] = nums[fastIndex]; 
+            }
+        }
+        return slowIndex;
+    }
+};
+
